3e65f58facea50f81dc770c82aadcbcb7ab8b6b7
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-19 09:56:51 +0330

    sadeghi

--- s2.py
+++ s2.py
@@ -1,1 +1,41 @@
-print("hello word")
+# This is our first program in python
+# print("hello world")
+# print("1+2")
+# print(2+3)
+# print('hello world')
+
+# print("your new score is:",100+10)
+# print("your new score is:","100+10")
+
+# print("he said \"some thing\"")
+# print("The file is stored in C:\new folder")
+# print("The file is stored in C:\\new folder")
+# x = 0
+# print(x)
+# print("x")
+# print("x =", x)
+# x = x + 1
+# print(x)
+
+# x = 5
+# x = 6
+# print(x)
+
+# #   +    -    *     /    //     %
+
+# print(5+5)
+# print(5-5)
+# print(5*5)
+# print(5/2)
+# print(5//2)
+# print(5 % 5)  # باقیمانده
+
+
+result = 5 + 2 * 3
+print(result)
+result = (5 + 2) * 3
+print(result)
+result = 5 + 2 / 3
+print(result)
+result = (5 + 2) / 3
+print(result)
