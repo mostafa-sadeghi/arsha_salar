aee4053db78b51ae6f163683362d33fe37321e05
msa <msai.sadehi@gmail.com>
2023-06-02 10:00:23 +0330

    sadeghi

--- s3.py
+++ s3.py
@@ -31,14 +31,24 @@
 # ave = (sc1 + sc2 + sc3) / 3
 
 # print("Your average is:", ave)
-name = input("Enter a name: ")
-course = ""
+# name = input("Enter a name: ")
+# course = ""
 # message = "Welcome " + name + " to python class"
-message = f"Welcome {name} to python class"
-print(message)
+# message = f"Welcome {name} to python class"
+# print(message)
 # exercise 1: به جای پایتون در متن بالا 
 # یک دوره دیگر را از ورودی دریافت کن و در متن قرار بده
 # + - * /
 # exercise 2: برنامه ای بنویس که دو عدد از ورودی دریافت نماید و حاصل ضرب ان ها را نمایش دهد
 # exercise 3 : برنامه ای بنویس که سه عدد از ورودی دریافت نماید و عدد اول و دوم را با هم جمع نموده
 #  از عدد سوم کم نماید 
+
+# number1 = float(input('Enter first number: '))
+# number2 = float(input('Enter second number: '))
+# number3 = float(input('Enter third number: '))
+
+# result = number1 + number2 - number3
+# print("result =", result)
+
+# x = "1" + " " + "ali"
+# print(x)
---
+++ s4.py
@@ -0,0 +1,17 @@
+a = 4
+b = 5
+
+if a > b:
+    print("a is greater than b")
+    print("ok")
+
+else:
+    print("a is less than or equal to b")
+
+
+# تمرین:
+# دو عدد از ورودی بگیری و با هم مقایسه کنی
+# نام، سن و نام کلاس برنامه نویس یمورد علاقه دو دانش آموز را از ورودی
+# بگیری و مثلا به صورت زیر نمایش دهی
+# arsha has 11 years old and likes python
+# artin has 10 years old and likes scratch
