5d7c4ced78a01fef1e000ab3b760e7deb9162888
msa <msai.sadehi@gmail.com>
2023-06-09 09:58:31 +0330

    sadeghi

--- s4.py
+++ s4.py
@@ -15,3 +15,11 @@ else:
 # بگیری و مثلا به صورت زیر نمایش دهی
 # arsha has 11 years old and likes python
 # artin has 10 years old and likes scratch
+
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+course = input('Enter your course name: ')
+# message = name + " has " + age + " years old and likes " + course + "."
+message = f"{name} has {age} years old and likes {course}"
+print(message)
---
+++ s5.py
@@ -0,0 +1,62 @@
+# number1 = int(input('Enter first number: '))
+# number2 = int(input('Enter second number: '))
+
+# if number1 == number2:
+#     print("equal")
+# elif number1 > number2:
+#     print("greater")
+# elif number1 < number2:
+#     print("less")
+
+# if number1 >= number2:
+#     print("greater than or equal")
+
+# if number1 <= number2:
+#     print("less than or equal")
+
+# if number1 != number2:
+#     print("number1 is not equalt to number2")
+#     print("ok")
+
+# print("outside")
+# exercise 1:
+# برنامه ای بنویسید که نام و نمرات سه درس یک دانش آموز را 
+# از ورودی دریافت نماید
+# معدل او را محاسبه و نمایش دهد
+# در صورتی که معدل او از نود بیشتر بود
+# "A" را پرینت کند
+# در صورتی که معدل او از هشتاد بیشتر بود
+# "B" را پرینت کند
+# در صورتی که معدل او از هفتاد بیشتر بود
+# "C" را پرینت کند
+# در غیر اینصورت
+# "D" را پرینت نماید
+
+# a = 12
+# b = 13
+# c = 10
+
+# if a > b and a > c:
+#     print("a is the largest")
+
+# if a > b or a > c:
+#     print("a is greater than b or c")
+
+a = True
+if a:
+    print("ok")
+
+a = False
+if a:
+    print("ok")
+
+if not a:
+    print("ok")
+
+a = True
+b = False
+if a and b:
+    print("ok")
+
+if a or b:
+    print("ok")
