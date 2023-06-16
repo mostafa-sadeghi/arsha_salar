5bcd2e18d34654dede6fab686ee7f9a66d564795
msa <msai.sadehi@gmail.com>
2023-06-16 09:56:51 +0330

    q

---
+++ s6.py
@@ -0,0 +1,42 @@
+# if 1:
+#     print("ok")
+# در شرطی که نوشتیم تمام اعداد غیر از صفر به عنوان درست در نظر گرفته می شوند
+
+# if 0:
+#     print("blalallal")
+
+
+# if True:
+#     print("bbbb")
+
+# if 2>1:
+#     print("cccc")
+
+# همه رشته ها غیر از رشته خالی درست در نظر گرفته می شوند
+
+# if "arsha":
+#     print("hello")
+
+# if "":
+#     print("bbbbbb")
+
+
+# user_name = input('enter user name: ').lower()
+
+# if user_name:
+#     password = input("enter password: ")
+#     if user_name == "arsha" and password == "12345":
+#         print("login success")
+#     else:
+#         print("user is not valid")
+
+# else:
+#     print("user name can not be empty")
+
+
+# name = "ArSha"
+# print(name.lower())
+
+
+# QUIZ
+# http://programarcadegames.com/quiz/quiz.php?lang=en&file=if_statements
