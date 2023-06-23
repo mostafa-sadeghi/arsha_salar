ea5fdeef689c99545e0c7b077a54d211325ed503
msa <msai.sadehi@gmail.com>
2023-06-23 09:59:13 +0330

    sadehgi

---
+++ s7.py
@@ -0,0 +1,25 @@
+# for i in range(5): # 0 1 2 3 4
+#     print(i)
+#     print("pygame")
+
+# print("outside")
+
+
+# for i in range(1,11): # 1 2 3 4 5 6 7 8 9 10
+#     print(i)
+
+
+# for i in range(2,11,2): # 2 4 6 8 10
+#     print(i , end=" ")
+
+
+# for i in range(1,11):
+#     print(i, end=" ")
+for i in range(10,0,-1):
+    print(i, end=" ")
+
+print()
+for i in range(10,0,-2):
+    print(i, end=" ")
+
+#  با کمک حلقه فور برنامه ای بنویس که پنج عدد از ورودی دریافت نماید و حاصلجمع اعداد را محاسبه و نمایش دهد
