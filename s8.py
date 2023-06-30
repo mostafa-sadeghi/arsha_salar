2982ab34151089d9452b3b2648880d7074a1ff61
msa <msai.sadehi@gmail.com>
2023-06-30 09:58:18 +0330

    flowchart

---
+++ s8.py
@@ -0,0 +1,55 @@
+
+
+# numbers = [2,4,5,6,7,8,9]
+# print(numbers)
+# print(numbers[0])
+# print(numbers[1])
+# print(numbers[-1])
+
+# names = ["sara", "artin", "roze", "mary"]
+# print(names)
+# print(names[0])
+# print(names[1])
+# print(names[-1])
+
+# for i in "python":
+#     print(i)
+
+# for i in [2,6,4,2,4,7,4]:
+#     print(i, end=" ")
+
+# for i in range(3):
+#     print("a")
+
+# for j in range(3):
+#     print("b")
+
+
+# for i in range(3):
+#     print("a")
+#     for j in range(3):
+#         print("b")
+# total = 0
+# for n in range(1, 10001):
+#     total += n
+
+# print(total)
+
+
+# TODO   برنامه ای بنویس که جمع اعداد زوج کمتر از 10000 را محاسبه و نمایش دهد
+
+
+# count = 0
+# for i in range(5):
+#     new_name = input("Enter a name:> ")
+#     if new_name == "sara":
+#         count += 1
+
+# print(f"sara repeated {count} times")
+
+# TODO برنامه ای بنویس که پنج عدد از ورودی دریافت نماید و تعداد اعداد زوج وارد شده 
+#  و نیز تعداد اعداد فرد وارد شده را نمایش دهد
+# عدد زوج عددی است که باقیمانده تقسیم آن بر دو مساوی صفر
+
+
+# https://app.diagrams.net/   ابزار آنلاین رسم فلوچارت
