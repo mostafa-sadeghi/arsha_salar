99cbcaaf80ee977b4e83cdef0ad080a26ab64af3
msa <msai.sadehi@gmail.com>
2023-07-07 09:57:23 +0330

    sadeghi

---
+++ s9.py
@@ -0,0 +1,96 @@
+# TODO برنامه ای بنویس که پنج عدد از ورودی دریافت نماید و تعداد اعداد زوج وارد شده 
+#  و نیز تعداد اعداد فرد وارد شده را نمایش دهد
+# عدد زوج عددی است که باقیمانده تقسیم آن بر دو مساوی صفر
+
+# even_counter = 0
+# odd_counter = 0
+# for i in range(5):
+#     new_number = int(input("enter a number: "))
+#     if new_number % 2 == 0:
+#         even_counter += 1
+#     else:
+#         odd_counter += 1
+
+# print(f"count number of evens: {even_counter}")
+# print(f"count number of odds: {odd_counter}")
+
+
+# my_variable = "python" + " " + "django"
+# print(my_variable)
+
+
+# my_variable = "python " + str(1) + " django" 
+# print(my_variable)
+
+# my_variable = "pygame " * 5
+# print(my_variable)
+
+# shopping_list = ["mashroom", "apple", "bananas","pencil"]
+
+# print(type(shopping_list))
+# print(shopping_list)
+# print("length of the list is: ",len(shopping_list))
+
+# print(shopping_list[0])
+# print(shopping_list[1])
+# print(shopping_list[2])
+# print(shopping_list[3])
+# print(shopping_list[4]) # error
+
+# shopping_list[3] = "potato"
+# print(shopping_list)
+
+# shopping_list.append("tomato")
+# print(shopping_list)
+
+
+# print(shopping_list[0:3])
+# print(shopping_list[1:3])
+
+# TODO  از لیست زیر برش های خواسته شده را ایجاد نمائید
+# my_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+#  ['a', 'b', 'c']
+#  ['a', 'b']
+#  ['d', 'e', 'f']
+#  ['f', 'g']
+#  ['b', 'c', 'd']
+
+
+# my_list = [1,2,3,4,5,0,100,234]
+# my_list[7] = 1000
+# my_list[-1] = 1000
+# my_list[len(my_list) - 1] = 1000
+# print(my_list)
+
+
+
+# my_list = [1,2,3,4,5,0,100,234, "milad", [1,2]]
+
+
+# x = 12
+# y = 13
+
+# z = x + y
+# print(z)
+
+
+# x = '12'
+# y = '13'
+# z = x + y
+# print(z)
+
+# x = [12]
+# y = [13]
+# z = x + y
+# print(z)
+
+numbers = []
+
+for i in range(4):
+    new_number = int(input("enter a number: "))
+    numbers.append(new_number)
+
+print("my list is:", numbers)
+
+# TODO  برنامه ای بنویس که پنج عدد از ورودی دریافت نماید و فقط اعداد فرد را در لیست اضافه نماید.
