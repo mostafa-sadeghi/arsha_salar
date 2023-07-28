3a729c57eeaa181c863bf64ee3133d77cff2a127
msa <msai.sadehi@gmail.com>
2023-07-28 09:53:15 +0330

    turtle

---
+++ session12..py
@@ -0,0 +1,51 @@
+import turtle
+
+window = turtle.Screen()
+window.bgcolor("orange")
+# window.bgpic('background.png')
+# window.register_shape('strawberry.gif')
+
+my_turtle = turtle.Turtle()
+my_turtle.shape('turtle') # 'arrow', 'turtle', 'circle', 'square', 'triangle', 'classic
+# my_turtle.shape('strawberry.gif')
+my_turtle.shapesize(2)
+my_turtle.speed('slow') # 'fastest', 'fast', 'normal', 'slow', 'slowest
+my_turtle.pensize(4)
+my_turtle.color('green')
+# my_turtle.penup()
+
+my_turtle.begin_fill()
+# for i in range(3):
+#     my_turtle.forward(100)
+#     my_turtle.left(120)
+# my_turtle.end_fill()
+#
+# my_turtle.color('red')
+# my_turtle.begin_fill()
+# i = 0
+# while i < 3:
+#     my_turtle.forward(100)
+#     my_turtle.right(120)
+#     i +=1
+# my_turtle.end_fill()
+
+
+# my_turtle.penup()
+# my_turtle.goto(-100,200)
+# for i in range(50):
+#     my_turtle.left(15)
+
+# TODO
+"""
+تمرین اول:
+کشیدن مربع با کمک حلقه فور و نیز حلقه وایل
+کشیدن پنجضلعی با کمک حلقه فور و نیز حلقه وایل
+کشیدن شش ضلعی با کمک حلقه فور و نیز حلقه وایل
+کشیدن مستطیل
+کشیدن ستاره
+
+"""
+
+
+
+turtle.done()
