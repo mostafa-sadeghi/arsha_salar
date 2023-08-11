9d103db068f62c40a2d5ec37e30510c64579e6c9
msa <msai.sadehi@gmail.com>
2023-08-11 09:59:27 +0330

    snake

--- snake_game.py
+++ snake_game.py
@@ -1,4 +1,6 @@
 from turtle import Screen, Turtle
+from random import randint
+from time import sleep
 
 def create_screen():
     my_window = Screen()
@@ -15,9 +17,49 @@ def create_turtle(tshape, tcolor):
     my_turtle.speed('fastest')
     return my_turtle
 
+def change_position(object):
+    x = randint(-270, 270)
+    y = randint(-270, 230)
+    object.goto(x,y)
+
+def move():
+    if snake_head.direction == "up":
+        y = snake_head.ycor()
+        y += 20
+        snake_head.sety(y)
+
+def go_up():
+    snake_head.direction = "up"
+
+def go_down():
+    snake_head.direction = "down"
+
+def go_right():
+    snake_head.direction = "right"
+
+def go_left():
+    snake_head.direction = "left"
 main_screen = create_screen()
-snake_head = create_turtle("square","blue")
+snake_head = create_turtle("square","green")
+snake_head.direction = ""
+snake_food = create_turtle("circle", "red")
+change_position(snake_food)
+
+main_screen.listen()
+main_screen.onkeypress(go_up, "Up")
+main_screen.onkeypress(go_up, "w")
+
+main_screen.onkeypress(go_down, "Down")
+main_screen.onkeypress(go_down, "s")
+
+main_screen.onkeypress(go_right, "Right")
+main_screen.onkeypress(go_right, "d")
+
+main_screen.onkeypress(go_left, "Left")
+main_screen.onkeypress(go_left, "a")
 
 running = True
 while running:
     main_screen.update()
+    move()
+    sleep(0.2)
